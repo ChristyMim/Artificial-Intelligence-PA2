b680c93ed7b83c05f57124a099b576f50c4cfefe
ChristyMim <christy.mims@usm.edu>
2019-11-04 17:54:48 -0600

    removed val imgs and added functions

--- PA2Files/DigitClassifier.py
+++ PA2Files/DigitClassifier.py
@@ -4,7 +4,37 @@ import math
 class Digit_Classifier(object):
 
     def __init__(self):
-        # size of images
-        self.grid_size: 28
-        self.train
+        self.size_height: 28
+        self.size_width: 28
+        self.training_size = 500
+        self.testing_size = 100
+        self.count = 0
 
+    def features(self, data):
+        """
+        returns whether pixel features are foreground or background
+        """
+        a = self.get_pixels()
+
+        features = self.count()
+        for i in range(self.size_width):
+            for j in range(self.size_height):
+                if self.get_pixel(i, j) > 0:
+                    features = [(i, j)] = 1
+                else:
+                    features = [(i, j)] = 0
+        return features
+
+    def train(self, train_images, train_labels):
+        open_train_images = open(train_images, 'r')
+        open_train_labels = open(train_labels, 'r')
+
+    def test(self, test_images, test_labels):
+        open_test_images = open(test_images, 'r')
+        open_test_labels = open(test_labels, 'r')
+
+    def get_pixel(self, column, row):
+        return self.pixels[column][row]
+
+    def get_pixels(self):
+        return self.pixels
