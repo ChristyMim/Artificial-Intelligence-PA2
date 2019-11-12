3e1bce2acbb1f58b7a73e6e8afa14f6aa21e62f3
ChristyMim <christy.mims@usm.edu>
2019-11-12 16:19:19 -0600

    deleted files

--- PA2Files/DigitClassifier.py
+++ PA2Files/DigitClassifier.py
@@ -1,11 +1,13 @@
 import math
 
+#SIZE_HEIGHT = 28
+#SIZE_WIDTH = 28
 
 class Digit_Classifier(object):
 
     def __init__(self):
-        self.size_height: 28
-        self.size_width: 28
+        self.size_height = 28
+        self.size_width = 28
         self.training_size = 500
         self.testing_size = 100
         self.count = 0
@@ -28,6 +30,16 @@ class Digit_Classifier(object):
     def train(self, train_images, train_labels):
         open_train_images = open(train_images, 'r')
         open_train_labels = open(train_labels, 'r')
+        counter = 0
+        for k in range(0.1, 10, 0.1):
+            for data_labels in open_train_labels:
+                counter += 1
+                data_labels = data_labels.rstrip('\n')
+                sample = []
+                row = list()
+                for row in range(self.size_width):
+                    line = open_train_images.readline()
+
 
     def test(self, test_images, test_labels):
         open_test_images = open(test_images, 'r')
